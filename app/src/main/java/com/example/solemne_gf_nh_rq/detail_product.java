ad72b99bec858e40bef3e752204d8f88daa2dae5
NicolasHidalgoGalaz <nickolashidalgo@gmail.com>
2022-11-27 22:58:35 -0400

    update

--- app/src/main/java/com/example/solemne_gf_nh_rq/detail_product.java
+++ app/src/main/java/com/example/solemne_gf_nh_rq/detail_product.java
@@ -6,6 +6,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.TextView;
 import android.widget.Toast;
 
@@ -27,6 +28,7 @@ public class detail_product extends AppCompatActivity {
     TextView textView2, textView3;
     Bundle bundle;
     Integer id;
+    Button delete_product;
 
     private RequestQueue rq;
 
@@ -36,6 +38,7 @@ public class detail_product extends AppCompatActivity {
         setContentView(R.layout.activity_detail_product);
         textView2 = findViewById(R.id.textView2);
         textView3 = findViewById(R.id.textView3);
+        delete_product = findViewById(R.id.delete_product);
 
         bundle = getIntent().getExtras();
         id = bundle.getInt("idItem");
@@ -60,6 +63,39 @@ public class detail_product extends AppCompatActivity {
                             } catch (JSONException e) {
                                 e.printStackTrace();
                             }
+                        }
+                    }
+                }, new Response.ErrorListener() {
+            @Override
+            public void onErrorResponse(VolleyError error) {
+
+            }
+        });
+        rq.add(requerimiento);
+    }
+
+    public void eliminarProducto(View view){
+        String idPro = id.toString();
+        String url = "http://10.0.2.2:3000/api/productos/" + idPro;
+        JSONObject parametros = new JSONObject();
+        try {
+            parametros.put("id_producto", id);
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+        JsonObjectRequest requerimiento = new JsonObjectRequest(Request.Method.DELETE, url, parametros,
+                new Response.Listener<JSONObject>() {
+                    @Override
+                    public void onResponse(JSONObject response) {
+                        try {
+                            String resu=response.get("id_producto").toString();
+                            if (resu.equals("1")){
+                                Toast.makeText(detail_product.this, "Se eliminó el producto", Toast.LENGTH_SHORT).show();
+
+                            } else
+                                Toast.makeText(detail_product.this, "No existe codigo", Toast.LENGTH_SHORT).show();
+                        } catch (JSONException e) {
+                            e.printStackTrace();
                         }
                     }
                 }, new Response.ErrorListener() {
